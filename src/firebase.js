03ae3f575de0f35f818ac883fcfa7cf7b3592bdb
DEVESH_JUYAL <91122298+DEVESHJUYAL@users.noreply.github.com>
2023-08-07 19:16:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-eoTWTjCscsYrCRm-Vh87qCg2kWZsDwY",
-    authDomain: "clone-4beed.firebaseapp.com",
-    projectId: "clone-4beed",
-    storageBucket: "clone-4beed.appspot.com",
-    messagingSenderId: "976156549625",
-    appId: "1:976156549625:web:c7a1dea75dde8361129d28"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
